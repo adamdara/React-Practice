68f4d2a5a629fb42dd496fe94b7bd406fa1aa68b
Darci01 <adam.dara@hotmail.com>
2022-10-18 19:15:54 +0200

    Filtering State

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends React.Component{
   constructor(){
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
   
@@ -19,6 +20,10 @@ class App extends React.Component{
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
     return (
       <div className="App">
       <input
@@ -26,7 +31,7 @@ class App extends React.Component{
         placeholder='search monsters'
         onChange={e => this.setState({searchField: e.target.value})}
       />
-      <CardList monsters={this.state.monsters}>
+      <CardList monsters={filteredMonsters}>
       </CardList>
       </div>
     );
